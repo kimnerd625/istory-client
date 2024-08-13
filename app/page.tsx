b1704190cf86508ed46a431d7f7e9c83b8f26b52
jihunkim625 <jihunkim625@yonsei.ac.kr>
2024-08-13 14:39:24 +0900

    ✨ feat: implement Header, BottomNavigationBar component and install @svgr/webpack library

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import BottomNavigationBar from "./sections/BottomNavigationBar";
+import Header from "./sections/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>하이, 세상아</h2>
+    <main className="w-full min-h-screen flex flex-col justify-start items-center">
+      <Header />
+      <BottomNavigationBar />
     </main>
   );
 }
---
+++ app/sections/BottomNavigationBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import HomeIcon from "@/public/icons/icon-home.svg";
+import BoxIcon from "@/public/icons/icon-box.svg";
+import ArchiveIcon from "@/public/icons/icon-archive.svg";
+import AccountIcon from "@/public/icons/icon-account.svg";
+import Link from "next/link";
+
+const BottomNavigationBar = () => {
+  return (
+    <section className="w-full flex flex-row justify-between items-center py-4 px-11">
+      <Link href="/">
+        <div className="flex flex-row justify-center items-center">
+          <HomeIcon width={24} height={24} />
+        </div>
+      </Link>
+      <Link href="/">
+        <div className="flex flex-row justify-center items-center">
+          <BoxIcon width={24} height={24} />
+        </div>
+      </Link>
+      <Link href="/">
+        <div className="flex flex-row justify-center items-center">
+          <ArchiveIcon width={24} height={24} />
+        </div>
+      </Link>
+      <Link href="/">
+        <div className="flex flex-row justify-center items-center">
+          <AccountIcon width={24} height={24} />
+        </div>
+      </Link>
+    </section>
+  );
+};
+
+export default BottomNavigationBar;
