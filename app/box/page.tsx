1c1c3cb0aa3ba74f715a8046a259d0bfca1d9e06
jihunkim625 <jihunkim625@yonsei.ac.kr>
2024-08-14 10:14:59 +0900

    ♻️ refactor: change BoxPage to fit in code convention

--- app/box/page.tsx
+++ app/box/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import BoxHero from "../components/box/BoxHero";
 
-const BoxPage = () => {
+export default function BoxPage() {
   return (
     <main className="w-full flex-1 flex flex-col justify-start items-center bg-home-bg">
       <BoxHero />
     </main>
   );
-};
-
-export default BoxPage;
+}
