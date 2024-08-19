b660414d833ba3a4d8d783d3b3e991aadabdc0c1
kimnerd625 <jihunkim625@yonsei.ac.kr>
2024-08-19 15:07:46 +0900

    ✨ feat: create ArchiveDetail page

---
+++ app/archive/[detail]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "@/app/sections/Header";
+import BoxInfo from "@/app/components/box/BoxInfo";
+
+export default function DetailPage() {
+  return (
+    <>
+      <Header isCancel={true} isCheck={false} isUser={false} />
+      <main className="w-full px-5 py-7 flex-1 flex flex-col justify-start items-center bg-home-bg gap-y-3">
+        <BoxInfo isDateVisible={false} />
+      </main>
+    </>
+  );
+}
--- app/components/archive/ArchiveCard.tsx
+++ app/components/archive/ArchiveCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import localFont from "next/font/local";
+import Link from "next/link";
 
 interface ArchiveCardProps {
   missionNo: number;
@@ -9,13 +10,15 @@ const EFDiary = localFont({ src: "../../../public/fonts/EFDiary.ttf" });
 
 const ArchiveCard = ({ missionNo }: ArchiveCardProps) => {
   return (
-    <div className="min-h-[50px] min-w-[50px] flex-1 aspect-square flex flex-row justify-start items-start cursor-pointer border border-[#CADFFF] rounded-lg px-[7px] py-[7px]">
-      <span
-        className={`${EFDiary.className} font-normal text-sm leading-5 text-[#CADFFF]`}
-      >
-        {missionNo}
-      </span>
-    </div>
+    <Link href="/archive/memo">
+      <div className="min-h-[50px] min-w-[50px] flex-1 aspect-square flex flex-row justify-start items-start cursor-pointer border border-[#CADFFF] rounded-lg px-[7px] py-[7px]">
+        <span
+          className={`${EFDiary.className} font-normal text-sm leading-5 text-[#CADFFF]`}
+        >
+          {missionNo}
+        </span>
+      </div>
+    </Link>
   );
 };
 
