c6a9cad169747927fa1f8c2414433835bf81594b
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-08-24 22:44:17 +0900

    ✨ feat: add state variables

--- app/components/account/InquiryInfoCard.tsx
+++ app/components/account/InquiryInfoCard.tsx
@@ -1,23 +1,33 @@
-import React, { SetStateAction } from "react";
+import React, { useState, useEffect } from "react";
 
-interface InquiryinfoCardProps {
-  accountNo: string;
+interface PaymentInfo {
+  date: string; // 납입 날짜
+  amount: string; // 납입 금액
 }
 
+interface InquiryinfoCardProps {
+  depositBalance: string; // 가입금액
+  totalBalance: string; // 누적금액
+  paymentInfo: PaymentInfo[]; // 납입 회차 정보 리스트
 
-const InquiryinfoCard = () => {
+}
+
+const InquiryinfoCard = ({depositBalance, totalBalance, paymentInfo }: InquiryinfoCardProps) => {
+
+  // 날짜 함수
+  const today = new Date();
+  const formattedDate = `${today.getFullYear()}. ${today.getMonth() + 1}. ${today.getDate()}`;
+
 
   return (
-    <div className="w-full  px-5 py-4 flex justify-start items-start rounded-[12px] border border-[#E2E2E2] bg-white shadow-[0_0_12px_0_rgba(0,0,0,0.10)]">
-      <div className="flex flex-col">
-          <span className = "text-gray-500 text-4 font-semibold">
-            더드림정기예금
-          </span>
-          <span className = "text-gray-400 text-[14px] font-light">
-          영업점 방문 없이 장기 목돈굴리기 상품
-          빠르게 가입해보세요!
-          </span>
-        </div>
+    <div className="w-full p-5 flex justify-start items-start">
+      <div className="w-full flex flex-col">
+          <span className="text-gray-400 font-light">{paymentInfo[0].date}</span>
+          <div className="flex flex-row justify-between mt-3">
+            <p>한 주 추억 쌓기</p>
+            <span className="text-main-600 text-xl font-semibold">{depositBalance}원</span>
+          </div>
+      </div>
     </div>
   );
 };
