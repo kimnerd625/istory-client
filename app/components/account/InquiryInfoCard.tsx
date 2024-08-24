1c94fa6a8c3e188dc5098aa843860dc0362b079a
gayoung000 <167841723+gayoung000@users.noreply.github.com>
2024-08-24 12:08:50 +0900

    ✨ feat: create Account Inquiry page

---
+++ app/components/account/InquiryInfoCard.tsx
@@ -0,0 +1,25 @@
+import React, { SetStateAction } from "react";
+
+interface InquiryinfoCardProps {
+  accountNo: string;
+}
+
+
+const InquiryinfoCard = () => {
+
+  return (
+    <div className="w-full  px-5 py-4 flex justify-start items-start rounded-[12px] border border-[#E2E2E2] bg-white shadow-[0_0_12px_0_rgba(0,0,0,0.10)]">
+      <div className="flex flex-col">
+          <span className = "text-gray-500 text-4 font-semibold">
+            더드림정기예금
+          </span>
+          <span className = "text-gray-400 text-[14px] font-light">
+          영업점 방문 없이 장기 목돈굴리기 상품
+          빠르게 가입해보세요!
+          </span>
+        </div>
+    </div>
+  );
+};
+
+export default InquiryinfoCard;
