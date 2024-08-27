5271282a30422e2ccffea3ba70fda5f58059e347
kimnerd625 <jihunkim625@yonsei.ac.kr>
2024-08-27 10:24:49 +0900

    💄 style: update login screen ui

--- app/components/LongButton.tsx
+++ app/components/LongButton.tsx
@@ -3,16 +3,27 @@ import React from "react";
 interface LongButtonProps {
   buttonText: string;
   buttonAction?: () => {};
+  color?: string;
 }
 
-const LongButton = ({ buttonText, buttonAction }: LongButtonProps) => {
+const LongButton = ({
+  buttonText,
+  buttonAction,
+  color = "blue",
+}: LongButtonProps) => {
   return (
     <div className="w-full flex flex-row justify-center items-center px-5">
       <button
         onClick={buttonAction}
-        className="w-full rounded-2xl py-[18px] flex flex-row justify-center items-center bg-main-400"
+        className={`w-full rounded-xl py-[18px] flex flex-row justify-center items-center ${
+          color == "blue" ? "bg-main-400" : "bg-white border border-[#C8C8C8]"
+        }`}
       >
-        <span className="text-xl tracking-tight leading-5 font-extrabold text-white">
+        <span
+          className={`text-xl tracking-tight leading-5 font-extrabold ${
+            color == "blue" ? "text-white" : "text-[#1E1E1E]"
+          }`}
+        >
           {buttonText}
         </span>
       </button>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,41 +1,61 @@
-"use client";
-import React, { useState } from "react";
-import Image from "next/image";
+import React from "react";
 import Link from "next/link";
 
 import LongButton from "../components/LongButton";
 
 export default function LoginPage() {
-  const [isLoginOpen, setIsLoginOpen] = useState<boolean>();
-
   return (
-    <main className="w-full flex-1 flex flex-col justify-start items-center bg-home-bg">
-      <section className="py-16 w-full flex flex-col justify-start items-center ">
-        <div className="w-full flex flex-col justify-center items-center gap-5">
-          <h4 className="text-xl font-bold text-black tracking-tight leading-5">
-            환영합니다!
-          </h4>
-          <h4 className="text-xl font-bold text-black tracking-tight leading-6 text-center">
-            추억과 함께 쌓이는 <br />
-            ISTORY
-          </h4>
+    <main className="w-full flex-1 flex flex-col justify-start items-center bg-home-bg overflow-x-hidden py-[56px]">
+      <section className="px-5 w-full flex-col justify-start items-start">
+        <h3 className="text-[#1A2128] font-bold text-[26px] leading-8 tracking-tight">
+          안녕하세요 !
+        </h3>
+        <h3 className="text-[#1A2128] font-bold text-[26px] leading-8 tracking-tight">
+          아이스토리 입니다.
+        </h3>
+        <h5 className="mt-3 text-sm text-[#777777] font-normal tracking-tight leading-4">
+          이메일을 입력해주세요.
+        </h5>
+      </section>
+      <section className="mt-[58px] px-5 w-full flex flex-col justify-start items-start gap-y-3">
+        <div className="w-full flex flex-col justify-center items-start gap-y-[7px]">
+          <label
+            className="font-semibold text-sm tracking-tight text-[#1A2128] leading-3"
+            htmlFor="email"
+          >
+            이메일
+          </label>
+          <input
+            id="email"
+            type="text"
+            placeholder="이메일을 입력해주세요."
+            className="text-base leading-4 tracking-tight font-normal text-[#777777] w-full focus:outline-none focus:border-none rounded-lg bg-[#E4E4E4] px-[14px] py-[13px] flex flex-row justify-start items-center"
+          />
         </div>
-        <div className="relative mt-16 w-[206px] h-[193px]">
-          <Image
-            src="/images/login-hero-character.png"
-            alt="로그인 캐릭터 이미지"
-            fill
+        <div className="w-full flex flex-col justify-center items-start gap-y-[7px]">
+          <label
+            className="font-semibold text-sm tracking-tight text-[#1A2128] leading-3"
+            htmlFor="password"
+          >
+            비밀번호
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            className="text-base leading-4 tracking-tight font-normal text-[#777777] w-full focus:outline-none focus:border-none rounded-lg bg-[#E4E4E4] px-[14px] py-[13px] flex flex-row justify-start items-center"
           />
         </div>
       </section>
-      <LongButton buttonText="로그인" />
-      <div className="mt-8 border-b border-[#757575]">
-        <Link href="/signup">
-          <span className="text-[#757575] text-sm tracking-tight leading-4 font-normal">
-            회원 가입
-          </span>
+      <section className="mt-[49px] flex flex-col justify-center items-center w-full gap-y-5">
+        <LongButton buttonText="로그인" />
+        <Link
+          className="w-full flex flex-row justify-center items-center"
+          href="/signup"
+        >
+          <LongButton buttonText="회원가입" color="white" />
         </Link>
-      </div>
+      </section>
     </main>
   );
 }
