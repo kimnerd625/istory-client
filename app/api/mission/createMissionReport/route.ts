88043d2cd9c862f75411fee94f6eddac1713dda5
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-08-28 14:41:44 +0900

    ✨ feat: implement mission api routes

---
+++ app/api/mission/createMissionReport/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+
+import { BASE_URL } from "../../base_url";
+
+export async function POST(request: Request) {
+  try {
+    const { email, famillymissionNo, thoughts } = await request.json();
+
+    const apiUrl = `${BASE_URL}/mission/report`;
+
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        user: { userId: email },
+        familymissionNo: famillymissionNo,
+        thoughts: thoughts,
+      }),
+    });
+
+    const contentType = response.headers.get("Content-Type");
+
+    let responseData;
+    if (contentType && contentType.includes("application/json")) {
+      responseData = await response.json();
+    } else {
+      responseData = await response.text();
+    }
+
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: "Error: " + responseData },
+        { status: response.status }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "미션 소감이 성공적으로 등록되었습니다.", data: responseData },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "미션 소감 등록이 실패했습니다: " + error.message },
+      { status: 500 }
+    );
+  }
+}
